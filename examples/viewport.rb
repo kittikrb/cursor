b5c37725a215de856c9d38058365c6a765b04265
Vladyslav Siriniok <siriniok@gmail.com>
2016-03-04 02:35:19 +0200

    Add viewport usage example

---
+++ examples/viewport.rb
@@ -0,0 +1,6 @@
+require 'kittikrb/cursor'
+
+cursor = KittikRb::Cursor.create.reset_tty!
+
+# It will print only 34567890 because you have negative X coordinate
+cursor.move_to(-2, 1).write('1234567890').flush
