6356d08ce95a6f5593e886b093677f503bdf7ca1
Vladyslav Siriniok <siriniok@gmail.com>
2016-05-30 12:21:22 +0300

    Simplify exapmles to single-threaded

--- examples/spin.rb
+++ examples/spin.rb
@@ -1,15 +1,5 @@
 require 'kittikrb/cursor'
 
-def set_interval(delay = 0)
-  Thread.new do
-    loop do
-      sleep delay
-
-      yield
-    end
-  end.join
-end
-
 cursor = KittikRb::Cursor.create.reset_tty!
 
 RADIUS = 10
@@ -18,7 +8,7 @@ COLORS = [:red, :yellow, :green, :olive, :blue, :magenta]
 points = []
 theta = 0
 
-set_interval do
+loop do
   x = 2 + (RADIUS + Math.cos(theta) * RADIUS) * 2
   y = 2 + RADIUS + Math.sin(theta) * RADIUS
 
@@ -30,4 +20,6 @@ set_interval do
 
   points = points.slice(0, 12 * COLORS.size - 1)
   theta += Math::PI / 40
+
+  sleep 0.001
 end
